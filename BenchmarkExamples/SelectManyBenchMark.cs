16f4ec4dd8c399c0d340bafb633a111a851da49e
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-08-06 03:49:21 +0530

    Removed unused usings

--- BenchmarkExamples/SelectManyBenchMark.cs
+++ BenchmarkExamples/SelectManyBenchMark.cs
@@ -2,12 +2,6 @@
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Reports;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BenchmarkExamples;
 
